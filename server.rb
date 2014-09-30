06696c7e76bbf7c45e88b73b7d59ebfd98839b3f
Henry Stanley <henry@henrystanley.com>
2014-09-30 19:50:50 +0100

    Updated main page to include BBC, CNN, et al

--- server.rb
+++ server.rb
@@ -5,6 +5,9 @@ require 'open-uri'
 
 get '/' do
   @guardian = SimpleRSS.parse open("http://www.theguardian.com/uk/rss")
-
+  @aljaz = SimpleRSS.parse open("http://www.aljazeera.com/Services/Rss/?PostingId=2007731105943979989")
+  @skynews = SimpleRSS.parse open("http://news.sky.com/feeds/rss/home.xml")
+  @beeb = SimpleRSS.parse open("http://feeds.bbci.co.uk/news/rss.xml?edition=uk")
+  @cnn = SimpleRSS.parse open("http://rss.cnn.com/rss/edition.rss")
   erb :index
 end
