93945abecf1be18b169a8ec136ee1ef8918f8ca4
Henry Stanley <henry@henrystanley.com>
2014-09-30 20:21:30 +0100

    Added bootstrap

--- server.rb
+++ server.rb
@@ -4,10 +4,14 @@ require 'simple-rss'
 require 'open-uri'
 
 get '/' do
+  get_the_news
+  erb :index
+end
+
+def get_the_news
   @guardian = SimpleRSS.parse open("http://www.theguardian.com/uk/rss")
   @aljaz = SimpleRSS.parse open("http://www.aljazeera.com/Services/Rss/?PostingId=2007731105943979989")
   @skynews = SimpleRSS.parse open("http://news.sky.com/feeds/rss/home.xml")
   @beeb = SimpleRSS.parse open("http://feeds.bbci.co.uk/news/rss.xml?edition=uk")
   @cnn = SimpleRSS.parse open("http://rss.cnn.com/rss/edition.rss")
-  erb :index
 end
